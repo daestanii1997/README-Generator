2a9e07f20f51c54d1ac8452bfe1e511b34017c2f
Dae <daestanii1997@gmail.com>
2023-08-23 10:43:05 -0600

    added remaining sections to readme file and prompts

--- Develop/index.js
+++ Develop/index.js
@@ -2,17 +2,39 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const writeToFile = ({name, description, usage, link}) =>
+const writeToFile = ({ name, description, installation, usage, license, contributing, tests, username, email }) =>
     `# ${name}
  
-     ## Desctiption
-     ${description}
- 
-     ## usage
-     ${usage}
- 
-     ## link
-     ${link} `;
+    ## Description
+    ${description}
+
+    ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+    ## Installation
+    ${installation}
+     
+    ## Usage
+
+    ${usage}
+
+    ## License
+    ${license}
+
+    ## Contributing
+    ${contributing}
+
+    ## Tests
+    ${tests}
+    
+    ## Questions
+    [Github](github.com/${username})
+    ${email} `;
 
 inquirer
     .prompt([
@@ -28,20 +50,46 @@ inquirer
         },
         {
             type: 'input',
+            name: 'installation',
+            message: 'Please enter any necessary installation instructions.',
+        },
+        {
+            type: 'input',
             name: 'usage',
             message: 'Provide instructions and examples of usage.',
         },
         {
             type: 'input',
-            name: 'link',
-            message: 'Please enter the link for your project.',
+            name: 'license',
+            message: 'Which license will you be using?',
+            choices: ['MIT', 'Apache 2.0', 'Boost Software', 'Eclipse Public 2.0'],
         },
-    ]) 
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Please enter the contribution guidelines.',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please enter test instructions.',
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Please enter your Github username.',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address.',
+        },
+    ])
     .then((answers) => {
 
         const readMeFile = writeToFile(answers);
 
         fs.writeFile('README.md', readMeFile, (err) =>
             err ? console.log(err) : console.log('A README file has been created!')
-            );
+        );
     }) 
